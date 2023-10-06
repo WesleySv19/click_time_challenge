e66fcd68aa7da166d0eec6ba672f2242bc578921
Wesley da Silva <wesleyysilvaa1912@gmail.com>
2023-10-06 15:21:57 -0300

    setting levels

--- js/game.js
+++ js/game.js
@@ -1,6 +1,5 @@
 const colors = ['#0000FF', '#000', '#FF0000', '#FFFF00', '#32CD32', '#8B008B', '#FF4500']
 const circle = document.createElement('div')
-
 const getRandomPosition = () => {
     let x = Math.floor(Math.random() * widthScreen) - 90
     let y = Math.floor(Math.random() * heightSreen) - 90
@@ -14,13 +13,25 @@ const getRandomColor = () => {
     return colors[randomIndex]
 }
 
+const dificult = () => {
+    const urlParams = new URLSearchParams(window.location.search)
+    const level = urlParams.get('level')
+    if(level === 'easy') {
+        return 'circleEasy'
+    } else if(level === 'medium') {
+        return 'circleMedium'
+    } else if(level === 'hard') {
+        return 'circleHard'
+    }
+}
+
 const updateCircle = () => {
+    circle.className = dificult()
     const position = getRandomPosition();
-    circle.className = 'circle'
     document.body.appendChild(circle)
     circle.style.backgroundColor = getRandomColor()
-    circle.style.left = position.x + 'px';
-    circle.style.top = position.y + 'px';
+    circle.style.left = position.x + 'px'
+    circle.style.top = position.y + 'px'
     circle.style.position = 'absolute'
     
 }
