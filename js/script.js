89d5e5bd676a26f2b0e172834e7d20736b26335e
Wesley da Silva <wesleyysilvaa1912@gmail.com>
2023-10-11 22:19:24 -0300

    showing feedback

--- js/script.js
+++ js/script.js
@@ -1,129 +1,31 @@
-let altura = 0
-let largura = 0
-let vidas = 1
-let tempo = 10
+const easy = document.querySelector('#easy')
+const medium = document.querySelector('#medium')
+const hard = document.querySelector('#hard')
+const initBtn = document.querySelector('#init')
+const stopwatch = document.querySelector('#stopwatch')
 
-let criaMosquitoTempo = 1500
+let heightSreen = 0
+let widthScreen = 0
+let count = 60
+let createsCircles = null
 
-let nivel = window.location.search
-nivel = nivel.replace('?', '')
 
-if (nivel === 'normal') {
-   criaMosquitoTempo = 1500
-
-} else if (nivel === 'dificil') {
-   criaMosquitoTempo = 1000
-
-} else if (nivel === 'chucknorris') {
-   criaMosquitoTempo = 750
+const adjustScreenSize = () => {
+   heightSreen = window.innerHeight
+   widthScreen = window.innerWidth
 }
 
-const ajustaTamanhoPalcoJogo = () => {
-   altura = window.innerHeight
-   largura = window.innerWidth
-}
-
-addEventListener('resize', ajustaTamanhoPalcoJogo())
+addEventListener('resize', adjustScreenSize())
 
 
-// function ajustaTamanhoPalcoJogo () {
-//    altura = window.innerHeight
-//    largura = window.innerWidth
-//    console.log(largura, altura)
-//    // console.log('teste')
 
-// }
-
-
-let cronometro = setInterval(function () {
-   tempo -= 1
-   if (tempo < 0) {
-      clearInterval(cronometro)
-      clearInterval(criaMosquito)
-      window.location.href = 'vitoria.html'
-   } else {
-      document.getElementById('cronometro').innerHTML = tempo
+let time = setInterval(() => {
+   stopwatch.innerHTML = count
+   count -= 1
+   if (count < 0) {
+      clearInterval(time)
+      document.querySelector('.resultPanel').style.display = 'block'
+      circle.style.display = 'none'
    }
 }, 1000)
 
-
-document.getElementById('cronometro').innerHTML = tempo
-
-posicaoRandomica()
-let criaMosquito = setInterval(function () {
-   posicaoRandomica()
-}, criaMosquitoTempo)
-
-function posicaoRandomica() {
-
-   // remover o mosquito anterior (caso exista)
-
-   if (document.getElementById('mosquito')) {
-      document.getElementById('mosquito').remove()
-
-      // console.log('O elemento selecionado foi' + vidas)
-
-      if (vidas > 3) {
-
-         window.location.href = 'fim_de_jogo.html'
-
-      } else {
-
-         document.getElementById('v' + vidas).src = "img/coracao_vazio.png"
-      }
-
-      vidas++
-   }
-
-
-   let posicaoX = Math.floor(Math.random() * largura) - 90
-   let posicaoY = Math.floor(Math.random() * altura) - 90
-
-   posicaoX = posicaoX < 0 ? 0 : posicaoX
-   posicaoY = posicaoY < 0 ? 0 : posicaoY
-
-   // console.log(posicaoX, posicaoY)
-
-   // criar elemento html
-   let mosquito = document.createElement('img')
-   mosquito.src = 'img/ball.png'
-   mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
-   mosquito.style.left = posicaoX + 'px'
-   mosquito.style.top = posicaoY + 'px'
-   mosquito.style.position = 'absolute'
-   mosquito.id = 'mosquito'
-   mosquito.onclick = function () {
-      this.remove()
-   }
-
-   document.body.appendChild(mosquito)
-
-   // console.log(ladoAleatorio())
-}
-
-function tamanhoAleatorio() {
-   let classe = Math.floor(Math.random() * 3)
-
-   switch (classe) {
-      case 0:
-         return 'mosquito1'
-
-      case 1:
-         return 'mosquito2'
-
-      case 2:
-         return 'mosquito3'
-   }
-}
-
-
-function ladoAleatorio() {
-   let classe = Math.floor(Math.random() * 2)
-   switch (classe) {
-      case 0:
-         return 'ladoA'
-
-      case 1:
-         return 'ladoB'
-   }
-}
