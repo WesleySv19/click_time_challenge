efa6426224ed8a94eba1fabfdcdb527380dd20d2
Wesley da Silva <wesleyysilvaa1912@gmail.com>
2023-11-29 00:36:03 -0300

    implementing new functions

--- src/js/game.js
+++ src/js/game.js
@@ -1,7 +1,9 @@
 const colors = ['#0000FF', '#FF0000', '#FFFF00', '#32CD32', '#8B008B', '#FF4500', '#FFF', '#8B4513', '#FF69B4']
 const circle = document.createElement('div')
-let clicks = 0
 const clickCount = document.querySelector('#result')
+const timeLeftDiv = document.querySelector('.time')
+const exitBtnDiv = document.querySelector('.exitBtn')
+let clicks = 0
 let timeUpdateCircle = 0
 
 const getRandomPosition = () => {
@@ -19,6 +21,11 @@ const getRandomColor = () => {
 
 const clickSound = () => {
     const audio = new Audio('./public/audio/click.mp3')
+    audio.play()
+}
+
+const clickSoundError = () => {
+    const audio = new Audio('./public/audio/error.mp3')
     audio.play()
 }
 
@@ -46,20 +53,54 @@ setInterval(() => {
 }, timeUpdateCircle)
 
 const updateCircle = () => {
-    circle.className = dificult()
-    const position = getRandomPosition();
-    document.body.appendChild(circle)
-    circle.style.backgroundColor = getRandomColor()
-    circle.style.left = position.x + 'px'
-    circle.style.top = position.y + 'px'
-    circle.style.position = 'absolute'
+    const margin = 90 
+    const position = getRandomPosition()
+
+    const isCircleOverlapping = (x, y, element) => {
+        const elementBounds = element.getBoundingClientRect()
+        return (
+            x + margin < elementBounds.right &&
+            x - margin > elementBounds.left &&
+            y + margin < elementBounds.bottom &&
+            y - margin > elementBounds.top
+        )
+    }
+
+    const isOverlappingTimeDiv = isCircleOverlapping(position.x, position.y, timeLeftDiv)
+    const isOverlappingExitBtnDiv = isCircleOverlapping(position.x, position.y, exitBtnDiv)
+
+    if (!isOverlappingTimeDiv && !isOverlappingExitBtnDiv) {
+        circle.className = dificult()
+        document.body.appendChild(circle)
+        circle.style.backgroundColor = getRandomColor()
+        circle.style.left = position.x + 'px'
+        circle.style.top = position.y + 'px'
+        circle.style.position = 'absolute'
+    }
 }
 
 
-circle.addEventListener('click', () => {
-    clicks += 1
-    updateCircle()
-    clickSound()
-    clickCount.innerHTML = clicks
-    
-});
+circle.addEventListener('click', (event) => {
+    event.stopPropagation()
+
+    const circleBounds = circle.getBoundingClientRect()
+    const clickX = event.clientX
+    const clickY = event.clientY
+
+    if (
+        clickX >= circleBounds.left &&
+        clickX <= circleBounds.right &&
+        clickY >= circleBounds.top &&
+        clickY <= circleBounds.bottom
+    ) {
+        clicks += 1
+        updateCircle()
+        clickSound()
+        clickCount.innerHTML = clicks
+    }
+})
+
+document.body.addEventListener('click', () => {
+
+    clickSoundError()
+})
